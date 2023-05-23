9f8fd963ccfe32d314ebcf081ba7c66598b2089e
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-05-23 10:52:31 +0100

    v1

---
+++ funciones.py
@@ -0,0 +1,15 @@
+def suma():
+    num1 = 6
+    num2 = 4
+    print(num1 + num2)
+
+
+def suma2(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+
+almacenaResultado = suma2(5, 10)
+
+print(almacenaResultado)
+suma()
