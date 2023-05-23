9c7663aedafd72141d238b8c623d62e7f7cf80ed
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-05-23 11:18:53 +0100

    v1

---
+++ listas.py
@@ -0,0 +1,8 @@
+milista = ["Juan", "Pepe", "Luis", "Ana"]
+
+milista.append("Gabriel")  # Agrega un elemento al final de la lista
+milista.insert(2, "Sandra")  # Agrega un elemento en la posición indicada
+# Agrega varios elementos al final de la lista
+milista.extend(["Manolo", "Andres", "Dua"])
+
+print(milista[:])  # Imprime toda la lista
