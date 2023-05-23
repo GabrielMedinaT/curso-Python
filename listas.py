e9c84badbbcf8b1e5c910e35dd573159289d1b97
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-05-23 11:25:22 +0100

    v3

--- listas.py
+++ listas.py
@@ -4,5 +4,10 @@ milista.append("Gabriel")  # Agrega un elemento al final de la lista
 milista.insert(2, "Sandra")  # Agrega un elemento en la posición indicada
 # Agrega varios elementos al final de la lista
 milista.extend(["Manolo", "Andres", "Dua"])
+milista.pop()  # Elimina el último elemento de la lista
 
+print(milista[:])  # Imprime toda la lista
+milista.remove("Ana")  # Elimina el elemento indicado
+milista.sort()  # Ordena la lista
+milista.reverse()  # Invierte el orden de la lista
 print(milista[:])  # Imprime toda la lista
